f68fa708f59e0858106f3d186625f1afcb901b9b
NicoP-IV <nicolasperry04@gmail.com>
2026-04-21 15:56:42 -0300

    Revert "Feature/menu"

--- src/main/java/domain/VehiculoElectrico.java
+++ src/main/java/domain/VehiculoElectrico.java
@@ -3,7 +3,8 @@ package domain;
 public class VehiculoElectrico extends Vehiculo {
     private double kwhBase;
 
-    public VehiculoElectrico(String patente, String marca, String modelo, int anio, double capacidadCarga,Sucursal sucursal, double kwhBase) {
+    public VehiculoElectrico(String patente, String marca, String modelo, int anio, double capacidadCarga,
+                             Sucursal sucursal, double kwhBase) {
         super(VehiculoTipo.ELECTRICO, patente, marca, modelo, anio, capacidadCarga, sucursal);
         this.kwhBase = kwhBase;
     }
